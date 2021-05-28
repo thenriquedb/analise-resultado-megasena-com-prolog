697046b969f1b59d731446bbe9beae242b7d679f
thiago <thenrique2012@gmail.com>
2021-05-28 10:46:52 -0300

    refactor structure

--- src/__main__.py
+++ src/__main__.py
@@ -4,8 +4,6 @@ import os
 import sys
 sys.path.append(os.path.normpath(os.path.join(os.getcwd(), "src")))
 
-# from server import Server
-
 if __name__ == "__main__":
     parser = ArgumentParser(description="Run server")
     parser.add_argument("csv", help="Your csv file")
@@ -16,10 +14,6 @@ if __name__ == "__main__":
     PORT = 4000
     CSV_PATH = args.csv
 
-    # server = Server(host="localhost", port=4000)
-
-    app = App(HOST, PORT, CSV_PATH)
+    app = App(HOST, PORT)
     app.load_games(CSV_PATH)
     app.start()
-
-    # server.run(args.csv)
--- src/server.py
+++ src/server.py
@@ -1,49 +1,29 @@
-from flask import Flask, request, render_template
-import pandas as pd
-from prolog_facade import PrologFacade
+from flask import Flask, render_template
+from controllers import Controllers
+
 
 class Server:
     def __init__(self, host, port):
-        self.__app = Flask(__name__, template_folder='../public')
+        self.__app = Flask(__name__, template_folder='./templates')
+        self.__app.config.update(
+            TESTING=True,
+            TEMPLATES_AUTO_RELOAD=True
+        )
         self.__host = host
         self.__port = port
-        self.__prolog = PrologFacade()
-
-    def __load_prolog(self, csv_file):
-        data = pd.read_csv(csv_file)
-
-        self.__prolog.consult("./src/analyze.pl")
-
-        for row in data.itertuples():
-            self.__prolog.assertz(
-                f"game({row.contest}, {row.number_01}, {row.number_02}, {row.number_03}, {row.number_04}, {row.number_05}, {row.number_06})")
+        self.__controllers = Controllers()
 
     def __register_routers(self):
         self.__app.add_url_rule("/", "index", self.__index)
-        self.__app.add_url_rule("/number-drawn", "number_drawn", self.__number_drawn,  methods=['POST'])
+        self.__app.add_url_rule(
+            "/number-drawn", "number_drawn", self.__number_drawn, methods=['POST'])
 
     def __index(self):
         return render_template('index.jinja-html')
 
     def __number_drawn(self):
-        body = request.get_json()
+        return self.__controllers.number_drawn()
 
-        if "number" not in body:
-            return { 
-                "data": {
-                    "number": ["Number is required"] 
-                }
-            }
-
-        drawn = self.__prolog.query("number_drawn({})".format(body['number']))
-
-        return {
-            "data": {
-                "drawn": True if drawn else False
-            }
-        }
-
-    def run(self, csv_file):
-        self.__load_prolog(csv_file)
+    def run(self):
         self.__register_routers()
         self.__app.run(host=self.__host, port=self.__port, debug=True)
