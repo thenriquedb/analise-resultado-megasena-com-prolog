90c2697c7dc8d7c7500a1431bbb02086951cd06c
Gabriel Augusto <gabrielfrzy@gmail.com>
2021-06-25 16:23:04 -0300

    add feature more_drawn_game

--- src/analyze.pl
+++ src/analyze.pl
@@ -1,11 +1,22 @@
 :- dynamic(game/7).
 
-game(1232, 1, 2, 7, 4, 8, 6) .
-game(8721, 10, 20, 32, 40, 50, 60) .
-game(2312, 1, 2, 8, 9, 3, 6) .
-game(2312, 1, 9, 5, 4, 10, 6) .
-game(2312, 1, 3, 7, 4, 5, 6) .
-game(2312, 1, 2, 9, 4, 5, 3) .
+game(1, 1, 2, 7, 4, 8, 6).
+game(2, 10, 20, 32, 40, 50, 60).
+game(3, 1, 2, 8, 9, 3, 6).
+game(4, 1, 9, 5, 4, 10, 6).
+game(5, 1, 3, 7, 4, 5, 6).
+game(6, 1, 2, 9, 4, 5, 3).
+game(7, 1, 3, 7, 4, 5, 6).
+game(8, 1, 9, 5, 4, 10, 6).
+game(9, 1, 9, 5, 4, 10, 6).
+
+create_list_of_numbers(A, A, [A]).
+create_list_of_numbers(N, A, [N|T]) :-
+    N < A,
+    N1 is N + 1,
+    create_list_of_numbers(N1, A, T).
+
+max_list(L, M, I) :- nth0(I, L, M), \+ (member(E, L), E > M).
 
 /*************************************************************************
  * O número X já foi sorteado alguma vez? Por exemplo: numero_sorteado(2).
@@ -22,10 +33,31 @@ number_drawn(X) :- game(_, X, _, _, _, _, _)  ;
 *************************************************************************/
 never_drawn(List) :- findall(X, (between(1, 60, X), not(number_drawn(X))), List).
 
-% O jogo (X1,X2,X3,X4,X5,X6) já foi contemplado alguma vez? Por exemplo: jogo_sorteado(2,3,5,7,9,19).
-winning_game(A,B,C,D,E,F) :- game(_,A,B,C,D,E,F) .
+/*************************************************************************
+ * O jogo (X1,X2,X3,X4,X5,X6) já foi contemplado alguma vez? Por exemplo: jogo_sorteado(2,3,5,7,9,19).
+*************************************************************************/
+winning_game(A,B,C,D,E,F) :- game(_,A,B,C,D,E,F).
 
-win_the_game_more_often() :- false.
+
+/*************************************************************************
+ * Algum jogo completo já foi contemplado mais de uma vez? Qual?
+*************************************************************************/
+create_list_of_game_count(List) :- findall(Q, 
+    (   
+        
+        game(_, N1, N2, N3, N4, N5, N6), 
+        aggregate_all(count, game(_, N1, N2, N3, N4, N5, N6), Q)
+    ), List).
+
+create_list_of_games(List) :- findall(G, game(G, _, _, _, _, _, _), List).
+
+more_drawn_game(X) :- 
+    create_list_of_game_count(ListC),
+    create_list_of_games(ListG), 
+    max_list(ListC, _, Index),
+    nth0(Index, ListG, E),
+    findall([N1, N2, N3, N4, N5, N6], (game(G, N1, N2, N3, N4, N5, N6), G =:= E), X).
+
 
 /*************************************************************************
  * Um número X foi sorteado quantas vezes?.
@@ -35,12 +67,5 @@ count_occ(X, N) :- aggregate_all(count, number_drawn(X), N).
 /*************************************************************************
 * Qual o número foi mais sorteado?
 ************************************************************************/
-pred(A, A, [A]).
-pred(N, A, [N|T]) :-
-    N < A,
-    N1 is N + 1,
-    pred(N1, A, T).
 
-max_list(L, M, I) :- nth0(I, L, M), \+ (member(E, L), E > M).
-
-more_drawn_number(X) :- pred(1, 60, L), maplist(count_occ, L, Result), max_list(Result, _, X).
+more_drawn_number(X) :- create_list_of_numbers(1, 60, L), maplist(count_occ, L, Result), max_list(Result, _, X).
