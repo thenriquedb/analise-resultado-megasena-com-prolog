b75fda3a49e5a515bd72c6a692163955be715e0a
thiago <thenrique2012@gmail.com>
2021-06-26 18:46:03 -0300

    delete mock games

--- src/analyze.pl
+++ src/analyze.pl
@@ -1,15 +1,5 @@
 :- dynamic(game/7).
 
-game(1, 1, 2, 7, 4, 8, 6).
-game(2, 10, 20, 32, 40, 50, 60).
-game(3, 1, 2, 8, 9, 3, 6).
-game(4, 1, 9, 5, 4, 10, 6).
-game(5, 1, 3, 7, 4, 5, 6).
-game(6, 1, 2, 9, 4, 5, 3).
-game(7, 1, 3, 7, 4, 5, 6).
-game(8, 1, 9, 5, 4, 10, 6).
-game(9, 1, 9, 5, 4, 10, 6).
-
 create_list_of_numbers(A, A, [A]).
 create_list_of_numbers(N, A, [N|T]) :-
     N < A,
